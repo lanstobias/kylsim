fdad0d566bd885df5369d67c6f6b47dab96086b5
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-05-12 16:21:48 +0100

    Revert "Byt namn på Form1 till FormKylsim"
    This reverts commit 28807e82e1e9766614c050224ff638ee7470ab45.

--- kylsim/Program.cs
+++ kylsim/Program.cs
@@ -16,7 +16,7 @@ namespace kylsim
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormKylsim());
+            Application.Run(new Form1());
         }
     }
 }
