b16defa937baa25575ca601475d19d03e9ec4a27
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-05-16 10:01:51 +0100

    Skapa klassen VVS
    En Kylsim fil följer med, dock är den utan innehåll.

---
+++ kylsim/VVS.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kylsim
+{
+    public class VVS
+    {
+
+        public string Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int W { get; set; }
+        public int H { get; set; }
+
+        public VVS Next{get; set;}
+        public VVS NodeIn { get; set; }
+        public VVS NodeOut { get; set; }
+        //Konstruktor
+        public VVS(string name="",int x=0, int y=0, int w=0, int h=0,
+            VVS next=null, VVS nodeIn=null, VVS nodeOut = null)
+        {
+            Name = name;
+            X = x;
+            Y = y;
+            W = w;
+            H = h;
+            Next = next;
+            NodeIn = nodeIn;
+            NodeOut = nodeOut;
+        }
+    }
+
+    public class Node: VVS
+    {
+
+    }
+
+    public class Valve : VVS
+    {
+
+    }
+    
+    public class Pump : VVS
+    {
+
+    }
+    public class HeatExchanger : VVS
+    {
+
+    }
+
+}
