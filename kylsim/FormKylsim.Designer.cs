9efef5fb018db662bbf841529039af53a2a5262e
BoboLillqvist <bobo.lillqvist@gmail.com>
2017-05-26 14:10:54 +0100

    Ändra tillbaks tidsintervallet

--- kylsim/FormKylsim.Designer.cs
+++ kylsim/FormKylsim.Designer.cs
@@ -39,7 +39,7 @@
             // timer
             // 
             this.timer.Enabled = true;
-            this.timer.Interval = 1000;
+            this.timer.Interval = 350;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
             // 
             // contextMenuStrip1
